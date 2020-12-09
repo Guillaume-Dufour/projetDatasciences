2f2ae64304a1b31f22791850866e722db743645c
Guillaume DUFOUR <46460244+Guillaume-Dufour@users.noreply.github.com>
2020-12-09 15:25:12 +0100

    Add new column weekday

--- main.py
+++ main.py
@@ -1,15 +1,21 @@
 from pandas import *
+from datetime import datetime
 
-df = pandas.read_csv("data.csv", sep=',', low_memory=False)
+df = pandas.read_csv("dataV2.csv", sep=',', low_memory=False)
+
+df['weekday'] = df.apply(lambda row: (datetime.fromisoformat(row.Date)).strftime("%A"), axis=1)
+
+df.to_csv('dataV3.csv', index=False)
 
 
+"""
 def compter_weight(i):
-    dataframe = df.groupby(by=['Cat_'+str(i)+'_weight'], dropna=False).count()
+    dataframe = df.groupby(by=['Cat_' + str(i) + '_weight'], dropna=False).count()
     print(dataframe)
 
 
 def compter_level(i, j):
-    dataframe = df.groupby(by=['Cat_'+str(i)+'_level_'+str(j)], dropna=False).count()
+    dataframe = df.groupby(by=['Cat_' + str(i) + '_level_' + str(j)], dropna=False).count()
     print(dataframe)
 
 
@@ -17,13 +23,16 @@ for x in range(1, 13):
     for y in range(1, 3):
         compter_level(x, y)
 """
+
+"""
 df2 = df.drop_duplicates(subset=["Date", "From", "To"])
 
-df2.to_csv('dataV2.csv')
-"""
+df2.to_csv('dataV2.csv', index=False)
 
-from datetime import datetime
+
 x = datetime.fromisoformat("2000-08-22 14:44:00")
 
 print(x.strftime("%A"))
 print(x.weekday())
+
+"""
