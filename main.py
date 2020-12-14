fe0206e4390f36a73da0ac614da1cf43d567ad33
MathildeTrb <mathilde.tribot@etu.umontpellier.fr>
2020-12-14 17:05:49 +0100

    maj main

--- main.py
+++ main.py
@@ -1,14 +1,33 @@
+from matplotlib import pyplot
 from pandas import *
+from matplotlib import *
 from datetime import datetime
+from Traffic import *
 
-df = pandas.read_csv("dataV2.csv", sep=',', low_memory=False)
-
-df['weekday'] = df.apply(lambda row: (datetime.fromisoformat(row.Date)).strftime("%A"), axis=1)
-
-df.to_csv('dataV3.csv', index=False)
+weeklyTraffic()
+yearTraffic()
 
 
 """
+
+df = pandas.read_csv("dataWeekDay.csv", sep=',', low_memory=False)
+
+df['year'] = df.apply(lambda row: (datetime.fromisoformat(row.Date)).strftime("%Y"), axis=1)
+df['hour'] = df.apply(lambda row: (datetime.fromisoformat(row.Date)).strftime("%H"), axis=1)
+
+df.to_csv('dataWeekDay.csv', index=False)"""
+
+"""
+df = pandas.read_csv("data.csv", sep=',', low_memory=False)
+
+df2 = df.drop_duplicates(subset=["Date", "From", "To"])
+
+df2 = df2.drop(df2[df2.Date < "2000-00-00 00:00:00"].index)
+
+df2['weekday'] = df2.apply(lambda row: (datetime.fromisoformat(row.Date)).strftime("%A"), axis=1)
+
+df2.to_csv('dataWeekDay.csv', index=False)
+
 def compter_weight(i):
     dataframe = df.groupby(by=['Cat_' + str(i) + '_weight'], dropna=False).count()
     print(dataframe)
@@ -22,13 +41,6 @@ def compter_level(i, j):
 for x in range(1, 13):
     for y in range(1, 3):
         compter_level(x, y)
-"""
-
-"""
-df2 = df.drop_duplicates(subset=["Date", "From", "To"])
-
-df2.to_csv('dataV2.csv', index=False)
-
 
 x = datetime.fromisoformat("2000-08-22 14:44:00")
 
