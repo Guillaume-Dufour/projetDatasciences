a694b8b7da483461bf04deca2d75787bd3ebbc69
MathildeTrb <mathilde.tribot@etu.umontpellier.fr>
2020-12-23 22:59:24 +0100

    ConstructeurCSV

--- Traffic.py
+++ Traffic.py
@@ -1,10 +1,10 @@
 from matplotlib import pyplot as plt
 from pandas import *
 
-def weeklyTraffic () :
-    df = pandas.read_csv("dataWeekDay.csv", sep=",", low_memory=False)
+def weeklyTraffic (csvIn) :
+    df = pandas.read_csv(csvIn, sep=",", low_memory=False)
     x = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
-    count_sorted_by_day = df.groupby(['weekday']).size().reindex(x)
+    count_sorted_by_day = df.groupby(['weekDay']).size().reindex(x)
 
     plt.bar(x, count_sorted_by_day, color='pink')
     plt.xlabel('jour de la semaine')
@@ -12,10 +12,9 @@ def weeklyTraffic () :
     plt.title('nombre de mail en fonction du jour de la semaine')
     plt.show()
 
-def yearTraffic ():
-    df = pandas.read_csv("dataWeekDay.csv", sep=",", low_memory=False)
+def yearTraffic (csvIn):
+    df = pandas.read_csv(csvIn, sep=",", low_memory=False)
     df_count = df.groupby(df['year']).size()
     print(df_count)
     plt.bar(df_count.index,df_count)
-    #TODO : signaler qu'il faut enlever tout ce qu'il y a au dessus de 2003
     plt.show()
--- main.py
+++ main.py
@@ -1,50 +1,11 @@
-from matplotlib import pyplot
-from pandas import *
-from matplotlib import *
-from datetime import datetime
 from Traffic import *
+from ConstructeurCSV import *
 
-weeklyTraffic()
-yearTraffic()
+dropLine("data.csv", "constructedData.csv")
+dropColumn("constructedData.csv", "constructedData.csv")
+addYear("constructedData.csv", "constructedData.csv")
+addHour("constructedData.csv", "constructedData.csv")
+addWeekDay("constructedData.csv", "constructedData.csv")
 
-
-"""
-
-df = pandas.read_csv("dataWeekDay.csv", sep=',', low_memory=False)
-
-df['year'] = df.apply(lambda row: (datetime.fromisoformat(row.Date)).strftime("%Y"), axis=1)
-df['hour'] = df.apply(lambda row: (datetime.fromisoformat(row.Date)).strftime("%H"), axis=1)
-
-df.to_csv('dataWeekDay.csv', index=False)"""
-
-"""
-df = pandas.read_csv("data.csv", sep=',', low_memory=False)
-
-df2 = df.drop_duplicates(subset=["Date", "From", "To"])
-
-df2 = df2.drop(df2[df2.Date < "2000-00-00 00:00:00"].index)
-
-df2['weekday'] = df2.apply(lambda row: (datetime.fromisoformat(row.Date)).strftime("%A"), axis=1)
-
-df2.to_csv('dataWeekDay.csv', index=False)
-
-def compter_weight(i):
-    dataframe = df.groupby(by=['Cat_' + str(i) + '_weight'], dropna=False).count()
-    print(dataframe)
-
-
-def compter_level(i, j):
-    dataframe = df.groupby(by=['Cat_' + str(i) + '_level_' + str(j)], dropna=False).count()
-    print(dataframe)
-
-
-for x in range(1, 13):
-    for y in range(1, 3):
-        compter_level(x, y)
-
-x = datetime.fromisoformat("2000-08-22 14:44:00")
-
-print(x.strftime("%A"))
-print(x.weekday())
-
-"""
+weeklyTraffic("constructedData.csv")
+yearTraffic("constructedData.csv")
