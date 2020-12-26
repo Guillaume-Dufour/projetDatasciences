f4cabae323b036976ca04971065ad8dde189e45f
MathildeTrb <mathilde.tribot@etu.umontpellier.fr>
2020-12-26 15:24:02 +0100

    maj Traffic

--- Traffic.py
+++ Traffic.py
@@ -1,20 +1,60 @@
 from matplotlib import pyplot as plt
+import plotly.graph_objects as go
 from pandas import *
 
-def weeklyTraffic (csvIn) :
-    df = pandas.read_csv(csvIn, sep=",", low_memory=False)
+def weeklyTraffic (df) :
     x = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
-    count_sorted_by_day = df.groupby(['weekDay']).size().reindex(x)
+    df_count = df.groupby(['weekDay']).size().reindex(x)
 
-    plt.bar(x, count_sorted_by_day, color='pink')
+    """plt.bar(x, count_sorted_by_day, color='pink')
     plt.xlabel('jour de la semaine')
     plt.ylabel('nombre de mail')
     plt.title('nombre de mail en fonction du jour de la semaine')
-    plt.show()
+    plt.show()"""
+    fig = go.Figure(
+        data=[go.Bar(
+            x=df_count.index,
+            y=df_count)],
+        layout_title_text="nombre de mail envoyé en fonction du jour de la semaine"
+    )
+    return fig
 
-def yearTraffic (csvIn):
-    df = pandas.read_csv(csvIn, sep=",", low_memory=False)
+def yearTraffic (df):
     df_count = df.groupby(df['year']).size()
-    print(df_count)
-    plt.bar(df_count.index,df_count)
+    """plt.bar(df_count.index, df_count)
+    plt.show()"""
+    fig = go.Figure(
+        data=[go.Bar(
+            x=df_count.index,
+            y=df_count)],
+        layout_title_text="nombre de mail envoyé en fonction de l'année"
+    )
+    return fig
+
+def monthTraffic (df, year) :
+    """
+    plt.bar(df_count.index, df_count)
     plt.show()
+    return plt"""
+    x = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+    dfFiltered = df.drop(df[df.year != year].index)
+    df_count = dfFiltered.groupby(dfFiltered['month']).size().reindex(x)
+    fig = go.Figure(
+        data=[go.Bar(
+            x=df_count.index,
+            y=df_count)],
+        layout_title_text="nombre de mail envoyé en fonction du mois de l'année sélectionnée"
+    )
+    return fig
+
+def hourTraffic (df) :
+    df_count = df.groupby(df['hour']).size()
+    """plt.bar(df_count.index, df_count)
+    plt.show()"""
+    fig = go.Figure(
+        data=[go.Bar(
+            x=df_count.index,
+            y=df_count)],
+        layout_title_text="nombre de mail envoyé en fonction de l'heure dans la journée"
+    )
+    return fig
--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 from Traffic import *
 from ConstructeurCSV import *
 
+"""
 dropLine("data.csv", "constructedData.csv")
 dropColumn("constructedData.csv", "constructedData.csv")
 addYear("constructedData.csv", "constructedData.csv")
 addHour("constructedData.csv", "constructedData.csv")
 addWeekDay("constructedData.csv", "constructedData.csv")
+addMonth("constructedData.csv", "constructedData.csv")
+"""
 
-weeklyTraffic("constructedData.csv")
-yearTraffic("constructedData.csv")
+dataFrame = pandas.read_csv("constructedData.csv", sep=',', low_memory=False)
+weeklyTraffic(dataFrame)
+yearTraffic(dataFrame)
+monthTraffic(dataFrame, 2001)
+hourTraffic(dataFrame)
