f7b3465316416e7f86bcad78d22fd27cb03cb2cf
aymericCOUPRIE <43545632+aymericCOUPRIE@users.noreply.github.com>
2021-01-09 09:27:04 +0100

    Update anova_tpsRep_job.py

--- anova_tpsRep_job.py
+++ anova_tpsRep_job.py
@@ -4,11 +4,6 @@ import seaborn as sns
 import statsmodels.api as sm
 from statsmodels.formula.api import ols
 
-"""dataFrame = pd.read_csv("data/data_annova_job_time_response.csv", sep=",", low_memory=False)
-dataFrame = dataFrame.drop(dataFrame.columns[[0, 1, 4]], axis=1)
-ax = sns.boxplot(x='JobLevel', y='strength', data=dataFrame, color='#99c2a2')
-plt.show()"""
-
 dataFrame = pd.read_csv("data/data_annova_job_time_response.csv", sep=",", low_memory=False)
 dataFrame["time_response"] = dataFrame["time_response"] / 36000
 #dataFrame = dataFrame[dataFrame.time_response < 1000]
