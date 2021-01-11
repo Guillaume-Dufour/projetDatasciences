da5359b083a833265b39660ad304f5d68d9871c8
marineteroitin <marine.teroitin@gmail.com>
2021-01-11 12:25:39 +0100

    content

--- dash/conclusion.py
+++ dash/conclusion.py
@@ -4,5 +4,42 @@ import dash_core_components as dcc
 
 content = html.Div([
     html.H4("Conclusion"),
+    html.P(
+        "P1",
+        style={'marginBottom': 20}
+    ),
+    html.P(
+        "P2",
+        style={'marginBottom': 20}
+    ),
+    html.P(
+        "P3",
+        style={'marginBottom': 30}
+    ),
     html.H4("Nos impressions sur le projet"),
+    html.P(
+        "P1",
+        style={'marginBottom': 20}
+    ),
+    html.P(
+        "P2",
+        style={'marginBottom': 20}
+    ),
+    html.P(
+        "P3",
+        style={'marginBottom': 20}
+    ),
+    html.P(
+        "P4",
+        style={'marginBottom': 20}
+    ),
+    html.P(
+        "P5",
+        style={'marginBottom': 20}
+    ),
+    html.P(
+        "P6",
+        style={'marginBottom': 20}
+    ),
+
 ])
